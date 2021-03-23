a6cf6d85c3ecae2a4d5658eac231765184a0bbb0
unknown <N4027@SKComms.net>
2021-03-23 17:36:49 +0900

    modify road.cpp

--- 99.example/1.road/cpp/road.cpp
+++ 99.example/1.road/cpp/road.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-int main() 
-{
-	return 0;
-}
+int main() {
+    int n = 5;
+    string road("RRRUDD");
 
+    int x = 1;
+    int y = 1;
+    int c = road.length();
+
+    for(int i=0;i < c; i++) {
+
+        char r = road[i];
+
+        if( (r == 'L') && (y != 1) ) {
+            y -= 1;
+        } else if( (r == 'R') && (y != n)) {
+            y += 1;
+        } else if( (r == 'U') && (x != 1)) {
+            x -= 1;
+        } else if( (r == 'D') && (x != n)) { 
+            x += 1;
+        } 
+
+    }
+
+    cout << "(x,y):" << x << "," << y << endl;
+
+    return 0;
+}
